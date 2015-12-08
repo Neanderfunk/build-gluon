6706e414dbd0f77988afb19533f1ccabb3ed227d
Benedikt Wildenhain <esperanto@benedikt-wildenhain.de>
2015-12-09 00:11:28 +0100

    Compile gluon

--- build.sh
+++ build.sh
@@ -7,4 +7,17 @@ if [ ! -d gluon ]; then
   cd gluon
   mkdir site
   git clone https://github.com/Neanderfunk/site-ffnef site
+else
+  cd gluon
 fi
+
+git pull
+(cd site && git pull)
+make update
+#make clean
+make -j1 GLUON_TARGET=ar71xx-generic GLUON_BRANCH=experimental
+#make manifest GLUON_BRANCH=experimental
+#contrib/sign.sh $SECRETKEY images/sysupgrade/experimental.manifest
+
+#rm -rf /where/to/put/this/experimental
+#cp -r images /where/to/put/this/experimental
