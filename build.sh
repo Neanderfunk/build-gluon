c3ed4c31f759dd43350620abecb7fd487bf32e9f
Benedikt Wildenhain <esperanto@benedikt-wildenhain.de>
2016-01-31 22:46:47 +0100

    only build met and rat

--- build.sh
+++ build.sh
@@ -7,8 +7,6 @@ branch="$2"
 
 cd $branch
 
-echo revision="$1" > build-info.txt
-
 if [ ! -d gluon ]; then
   git clone https://github.com/freifunk-gluon/gluon v2015.1
 else
@@ -29,18 +27,14 @@ fi
 
 cd gluon
 
-if [ -d "$HOME/ccache" ]; then
-  export PATH="$HOME/ccache:$PATH"
-fi
-
-for sitedir in ../site-ffnef/*; do
-#for sitedir in ../site-ffnef/ffnef-met; do
+#for sitedir in ../site-ffnef/*; do
+for sitedir in ../site-ffnef/ffnef-met ../site-ffnef/ffnef-rat; do
   cp $sitedir/modules.incomplete $sitedir/modules
   grep -v ^GLUON_SITE_FEEDS= ../site-ffho/modules >> $sitedir/modules
 
   outputdir=out/$(basename $sitedir)
   mkdir -p $outputdir
-  params="GLUON_SITEDIR=$PWD/$sitedir GLUON_OUTPUTDIR=$PWD/$outputdir GLUON_BRANCH=stable" # (sic, simply copy after testing)
+  params="GLUON_SITEDIR=$PWD/$sitedir GLUON_OUTPUTDIR=$PWD/$outputdir GLUON_BRANCH=$branch"
   echo $params
   make update $params
   #make GLUON_TARGET=ar71xx-generic $params clean V=s # really necessary?
@@ -49,7 +43,8 @@ for sitedir in ../site-ffnef/*; do
   	  ar71xx-generic ar71xx-nand mpc85xx-generic \
 	  x86-generic x86-kvm_guest x86-64 x86-xen_domu
   do
-      make GLUON_TARGET=$target $params V=s
+  	  #echo $revision $params
+      make GLUON_TARGET=$target $params V=s -j8
   done
   make manifest $params
 done
